de7a91788bbde28d9acfa30fa6efd234028d6cd7
Abdul-commits55 <abdulmoiz1755@gmail.com>
2025-11-28 21:32:30 +0500

    update anime project
    update anime project

--- Build an Anime Pics Generator project/script.js
+++ Build an Anime Pics Generator project/script.js
@@ -4,18 +4,24 @@ const animeImgEl = document.querySelector(".anime-img");
 const amineNameEl = document.querySelector(".anime-name");
 
 btnEl.addEventListener("click", async function () {
+  console.log("oiz")
   try {
+    //  animeImgEl.src =""v
+    //  const img = document.getElementById("myImg");
+    // animeImgEl.removeAttribute("src");
     btnEl.disabled = true;
     btnEl.innerText = "Loading...";
     amineNameEl.innerText = "Updating...";
     animeImgEl.src = "spinner.svg";
-    const response = await fetch("https://api.catboys.com/img");
-    const data = await response.json();
+    //  const response = await fetch("https://picsum.photos/200");
+    //  console.log()
+    //  const data = await response.json();
     btnEl.disabled = false;
     btnEl.innerText = "Get Anime";
     animeContainerEl.style.display = "block";
-    animeImgEl.src = data.url;
-    amineNameEl.innerText = data.artist;
+   animeImgEl.src = "https://picsum.photos/200?random=" + Date.now();
+
+    //  amineNameEl.innerText = data.artist;
   } catch (error) {
     console.log(error);
     btnEl.disabled = false;
