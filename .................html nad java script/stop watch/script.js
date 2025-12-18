dd947224ddecb95acbdcbc6ec5b00992a7d8b415
Abdul-commits55 <abdulmoiz1755@gmail.com>
2025-12-18 17:20:08 +0500

    code
    code

--- .................html nad java script/stop watch/script.js
+++ .................html nad java script/stop watch/script.js
@@ -3,27 +3,36 @@ let stopBtn = document.getElementById('stopBtn');
 let startBtn = document.getElementById('startBtn');
 let resetBtn = document.getElementById('resetBtn');
 
-let msec = 00;
-let secs = 00;
-let mins = 00;
+let msec = '00';
+let secs = '00';
+let mins = '00';
 
 let timerId = null;
+
+ 
 
 startBtn.addEventListener('click', function(){
     if(timerId !== null){
         clearInterval(timerId);
+        
     }
+        timerDisplay.classList.add('green')
+        timerDisplay.classList.remove('red')
     timerId = setInterval(startTimer, 10);
 });
 
 stopBtn.addEventListener('click', function(){
     clearInterval(timerId);
+            timerDisplay.classList.remove('green')
+            
+
 });
 
 resetBtn.addEventListener('click', function(){
     clearInterval(timerId);
     timerDisplay.innerHTML = `00 : 00 : 00`;
-    msec = secs = mins = 00;
+    msec = secs = mins = '00';
+    timerDisplay.classList.add('red')
 });
 
 function startTimer(){
@@ -37,11 +46,11 @@ mins++;
         }
     }
 
-    let msecString = msec < 10 ? `0${msec}` : msec;
-    let secsString = secs < 10 ? `0${secs}` : secs;
-    let minsString = mins < 10 ? `0${mins}` : mins;
+    let msecString = msec < 10 ? `${msec}` : msec;
+    let secsString = secs < 10 ? `${secs}` : secs;
+    let minsString = mins < 10 ? `${mins}` : mins;
     
 
     timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
-
+ 
 }
